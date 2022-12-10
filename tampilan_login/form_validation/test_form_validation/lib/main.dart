7326bf55d3cb4bb057a8df3e3e4781955b4228ed
malikmustafaa <malikmustafa72735@gmail.com>
2022-12-10 15:05:22 +0700

    first commit

---
+++ tampilan_login/form_validation/test_form_validation/lib/main.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: unnecessary_import, unused_import
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'FormPage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const FormPage(),
+    );
+  }
+}
